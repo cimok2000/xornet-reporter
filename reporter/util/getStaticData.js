feaf536845d7592c1218606c4ba312be0d84203d
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-06-17 18:41:55 +0300

    add extra validation for uuids

--- reporter/util/getStaticData.js
+++ reporter/util/getStaticData.js
@@ -1,12 +1,13 @@
 const si = require("systeminformation");
 const getLocation = require("../util/getLocation");
+const uuidRegex = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/;
 
 module.exports = async function getStaticData() {
   return new Promise(async (resolve) => {
     const data = await si.getStaticData();
     data.geolocation = await getLocation();
     data.system.uuid = data.uuid.hardware.replace(/-/g, "") || data.uuid.os.replace(/-/g, "");
-    if (data.system.uuid == "03000200040005000006000700080009"){
+    if (data.system.uuid == "03000200040005000006000700080009" || !uuidRegex.test(data.system.uuid)){
       data.system.uuid = data.uuid.os.replace(/-/g, "");
     }
     resolve(data);
