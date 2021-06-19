7b0dc70ca58d33c256944585b77c91b608bfda5c
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-06-19 14:18:53 +0300

    v0.0.20 release
    Co-Authored-By: N1kO23 <59277828+N1kO23@users.noreply.github.com>

--- reporter/install-linux/install.sh
+++ reporter/install-linux/install.sh
@@ -19,12 +19,12 @@ echo "Xornet uninstalled!"
 mkdir /etc/xornet
 
 # Download
-wget "https://github.com/Geoxor/Xornet/releases/download/v0.0.19/xornet-reporter-linux-v0.0.19.bin" -P /etc/xornet
-chmod +x /etc/xornet/xornet-reporter-linux-v0.0.19.bin
+wget "https://github.com/Geoxor/Xornet/releases/download/v0.0.20/xornet-reporter-linux-v0.0.20.bin" -P /etc/xornet
+chmod +x /etc/xornet/xornet-reporter-linux-v0.0.20.bin
 echo "Xornet reporter downloaded!"
 
 # Download service
-sudo wget "https://cdn.discordapp.com/attachments/806300597338767450/855106661081415750/xornet.service" -P /etc/systemd/system
+sudo wget "https://cdn.discordapp.com/attachments/806300597338767450/855738801019944980/xornet.service" -P /etc/systemd/system
 systemctl enable xornet
 echo "Xornet service downloaded!"
 
