7dcc196add081b65986f8a669aab9b59e3fd7bdf
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-06-20 12:42:32 +0300

    added example jsonc for locales & prepared v0.0.21

--- reporter/install-linux/install.sh
+++ reporter/install-linux/install.sh
@@ -19,12 +19,13 @@ echo "Xornet uninstalled!"
 mkdir /etc/xornet
 
 # Download
-wget "https://github.com/Geoxor/Xornet/releases/download/v0.0.20/xornet-reporter-linux-v0.0.20.bin" -P /etc/xornet
-chmod +x /etc/xornet/xornet-reporter-linux-v0.0.20.bin
+wget "https://github.com/Geoxor/Xornet/releases/download/v0.0.21/xornet-reporter-linux-v0.0.21.zip" -P /etc/xornet
+unzip /etc/xornet/xornet-reporter-linux-v0.0.21.zip ./
+chmod +x /etc/xornet/xornet-reporter-linux-v0.0.21.bin
 echo "Xornet reporter downloaded!"
 
 # Download service
-sudo wget "https://cdn.discordapp.com/attachments/806300597338767450/855738801019944980/xornet.service" -P /etc/systemd/system
+sudo wget "https://cdn.discordapp.com/attachments/806300597338767450/856105546789027920/xornet.service" -P /etc/systemd/system
 systemctl enable xornet
 echo "Xornet service downloaded!"
 
