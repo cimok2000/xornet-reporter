50923007c2f1e5d322fd98b63d76aad7fef60a22
Dominik Rutter <dominik.rutter@gmail.com>
2021-06-19 22:12:04 -0400

    New extensible logger

--- reporter/util/logger.js
+++ reporter/util/logger.js
@@ -7,61 +7,100 @@ const localeTable = (() => {
   return JSON.parse(rawJson);
 })();
 
-// Used to log information to the console
-class Localizable {
-    constructor(msg) {
-        this.msg = msg;
-    }    
-}
-let bruh = new Localizable("bruh");
+
+// DOCUMENTATION 
+/*
+
+[ Primary Shell, put everything in here. Don't be silly, wrap your willy
+
+    [ Msg shell, contains a message or data and optionally, a color
+        
+        "", Non-optional, contains text to be translated, on translation fail, will fallback and return the key itself (this is to enable using data)
+
+        [   Option Style shell, Only use if doing multiple styles
+
+            "" Style options, refer to https://www.npmjs.com/package/colors for color/style options
+        ]  
+    ]
+]
+
+// EXAMPLE
+logger.info(
+  [ 
+    ["send"],
+    [123, "bgCyan"],
+    ["load", ["bgRed", "white", "underline"]],
+  ]
+);
+Will output https://i.imgur.com/sg59cn0.png
+// EXAMPLE
+
+*/
+// DOCUMENTATION
 
 class Logger {
     stylize(msg, style) {
-        if (style === "") style = new Array("white");
-        for (let i = 0; i < style.length; i++) {
-            msg = colors[style[i]](msg)
+        if (typeof style == array) {
+            for (let i = 0; i < style.length; i++) {
+                msg = colors[style[i]](msg)
+            }
+        } else {
+            msg = colors[style](msg);
         }
       return msg;
     }
-    
-    info(msg, data = "", style = "") { // General Info Messages
+
+    processList(primaryKey, items) { // Used to translate keys into their correnspending json value, or return the key itself (for Displaying Data), upon failure to find a value for the key
+        let processedText = new String();
+        items.forEach(element => {
+            let temp = localeTable[primaryKey][element[0]];
+            let msg = (temp != undefined) ? temp : element[0];
+            if (element.length == 1) {
+                processedText += msg;
+            } else {
+                processedText += (this.stylize(msg,element[1]));
+            }
+        });
+        return processedText;
+    }
+
+    info(items) { // General Info Messages
         const prefix = this.stylize(localeTable.msgIden.info, ["bgGrey", "black"]);
         console.log(
             prefix,
-            (msg == "") ? msg : this.stylize(localeTable.infoMsg[msg], style), // Fallback to allow manual coloring on hyper complex msgs's
-            data
+            this.processList("infoMsg", items)
        )
     }
-    warn(msg, data = "", style = "") { // Warning Messages
+
+    warn(items) { // Warning Messages
         const prefix = this.stylize(localeTable.msgIden.warn, ["bgYellow", "black"]);
         console.log(
             prefix,
-            (msg == "") ? msg : this.stylize(localeTable.warnMsg[msg], style),
-            data
+            this.processList("warnMsg", items)
         )
     }
-    error(msg, data = "", style = "") { // Fatal Error Messages
+
+    error(items) { // Fatal Error Messages
         const prefix = this.stylize(localeTable.msgIden.err, ["bgRed", "black"]);
         console.log(
             prefix,
-            (msg == "") ? msg : this.stylize(localeTable.errMsg[msg], style),
-            data            
+            this.processList("errMsg", items)           
         )
     }
-    test(msg, data = "", style = "") { // Speedtest Messages
+
+    test(items) { // Speedtest Messages
         const prefix = this.stylize(localeTable.msgIden.test, ["bgBlue", "black"]);
         console.log(
             prefix,
-            (msg == "") ? msg : this.stylize(localeTable.testMsg[msg], style),
-            data            
+            this.processList("testMsg", items)            
         )
     }
-    con(msg, data = "", style = "") { // Connectivity Messages
+    
+    con(items) { // Connectivity Messages
         const prefix = this.stylize(localeTable.msgIden.con, ["bgCyan", "black"]);
         console.log(
             prefix,
-            (msg == "") ? msg : this.stylize(localeTable.conMsg[msg], style),
-            data 
+            this.processList("conMsg", items) 
         )
     }
 }
