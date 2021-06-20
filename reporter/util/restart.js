401f498bbf819b5948baf9b0221e05c8b0b5c00e
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-06-20 19:52:38 +0300

    support for linux & macos

--- reporter/util/restart.js
+++ reporter/util/restart.js
@@ -5,7 +5,19 @@ const ReporterSettings = require("../util/settings");
 // Create restart function
 module.exports = function shutdown(){
   return new Promise(resolve => {
-    if (ReporterSettings.allowRestart) exec('shutdown /r', (error, stdout, stderr) => resolve(stdout));
+    if (ReporterSettings.allowRestart) {
+      switch (process.platform) {
+        case 'win32':
+          var shutdownCommand = 'shutdown /r'
+          break;
+        case 'linux':
+        case 'darwin':
+          var shutdownCommand = 'sudo reboot'
+          break;
+      }
+
+      exec(shutdownCommand, (error, stdout, stderr) => resolve(stdout));
+    }
     else resolve();
   });
 }
--- reporter/util/shutdown.js
+++ reporter/util/shutdown.js
@@ -5,7 +5,19 @@ const ReporterSettings = require("../util/settings");
 // Create shutdown function
 module.exports = function shutdown(){
   return new Promise(resolve => {
-    if (ReporterSettings.allowShutdown) exec('shutdown /s', (error, stdout, stderr) => resolve(stdout));
+    if (ReporterSettings.allowShutdown) {
+      switch (process.platform) {
+        case 'win32':
+          var shutdownCommand = 'shutdown /s'
+          break;
+        case 'linux':
+        case 'darwin':
+          var shutdownCommand = 'sudo shutdown'
+          break;
+      }
+
+      exec(shutdownCommand, (error, stdout, stderr) => resolve(stdout));
+    }
     else resolve();
   });
 }
