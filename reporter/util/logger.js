1ebceb45aee1efcc5a880039624b310278cdbcda
Dominik Rutter <dominik.rutter@gmail.com>
2021-06-19 22:17:24 -0400

    Fix bug

--- reporter/util/logger.js
+++ reporter/util/logger.js
@@ -40,7 +40,8 @@ Will output https://i.imgur.com/sg59cn0.png
 
 class Logger {
     stylize(msg, style) {
-        if (typeof style == array) {
+        console.log(style);
+        if (Array.isArray(style)) {
             for (let i = 0; i < style.length; i++) {
                 msg = colors[style[i]](msg)
             }
