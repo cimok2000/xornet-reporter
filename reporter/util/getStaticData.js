d34312c1d276aeff5d04d4822774a46cf9dd0049
cimok2000 <cimok2000@gmail.com>
2021-06-25 00:34:15 +0100

    send settings.json to backend as staticData

--- reporter/util/getStaticData.js
+++ reporter/util/getStaticData.js
@@ -1,5 +1,6 @@
 const si = require("systeminformation");
 const getLocation = require("../util/getLocation");
+const ReporterSettings = require("./settings");
 const uuidRegex = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/;
 
 module.exports = async function getStaticData() {
@@ -7,6 +8,7 @@ module.exports = async function getStaticData() {
     const data = await si.getStaticData();
     data.geolocation = await getLocation();
     data.system.uuid = data.uuid.hardware.replace(/-/g, "") || data.uuid.os.replace(/-/g, "");
+    data.settings = ReporterSettings;
     if (data.system.uuid == "03000200040005000006000700080009" || !uuidRegex.test(data.system.uuid)) {
       data.system.uuid = data.uuid.os.replace(/-/g, "");
     }
