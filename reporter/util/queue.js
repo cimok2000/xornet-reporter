b23955b0c04eefebdd5a24616005844c001dfc51
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-07-14 07:51:49 +0300

    fixed list & broken font on xterm
    Co-Authored-By: Bluskript <52386117+Bluskript@users.noreply.github.com>

--- reporter/util/queue.js
+++ reporter/util/queue.js
@@ -11,7 +11,7 @@ module.exports = class Queue {
   append(item){
     this.queue.push(item);
     if (this.limit && this.size() > this.limit) {
-      this.queue.pop()
+      this.queue.shift()
     }
   }
 
