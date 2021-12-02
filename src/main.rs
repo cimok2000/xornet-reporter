05ff142cbf1837352ea7c35f304a833724f2f966
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-02 09:47:00 +0200

    create base structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Debug)]
+struct Reporter {
+    pub data_dollector: DataCollector,
+    pub version: String,
+}
+
+impl Reporter {
+    fn new() -> Self {
+        let data_dollector: DataCollector = DataCollector::new();
+        let version: String = env!("CARGO_PKG_VERSION").to_string();
+
+        return Self {
+            data_dollector,
+            version,
+        };
+    }
+}
+
+#[derive(Debug)]
+
+struct DataCollector {}
+
+impl DataCollector {
+    fn new() -> Self {
+        return Self {};
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let reporter: Reporter = Reporter::new();
+    println!("{:?}", reporter);
 }
