1b64c15c28aa1b27cdd85e67ae207de1387aa9f3
Natapat Samutpong <natapat.samutpong@gmail.com>
2021-12-04 16:08:01 +0700

    some pretty formatting

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,38 @@ use std::{
     sync::Arc,
     thread::{self, spawn},
 };
+use colored::Colorize;
 
 mod data_collector;
 mod reporter;
 use crate::reporter::Reporter;
 
 fn main() {
+
+    // Prefix of the display thing (can also be in config)
+    let prefix = "●";
+
     let reporter = Arc::new(Mutex::new(Reporter::new()));
 
     // Get all static shit
-    println!("{}", reporter.lock().data_collector.get_statics());
+    println!("{} Info: {}", prefix.white(), reporter.lock().data_collector.get_statics());
 
     // Todo: make these run on a loop with unique intervals for each
     // that the user can set in a config
     let reporter = reporter.clone();
     let data_collection_handle = spawn(move || loop {
         let mut reporter = reporter.lock();
-        println!("{}", reporter.data_collector.get_disks());
-        println!("{}", reporter.data_collector.get_cpu());
-        println!("{}", reporter.data_collector.get_ram());
-        println!("{}", reporter.data_collector.get_network());
+
+        println!("{} Disks: {}", prefix.red(),
+                reporter.data_collector.get_disks()[0].get("free").expect("Error in CPU displaying."));
+        println!("{} Ram: {} / {}", prefix.green(),
+                reporter.data_collector.get_ram().get("available_memory").expect("Erorr in RAM displaying."),
+                reporter.data_collector.get_ram().get("total_memory").expect("Error in RAM displaying."));
+        println!("{} Network: {}", prefix.blue(),
+                reporter.data_collector.get_network()[0]);
+        println!("{} CPU: {:.2}%", prefix.magenta(),
+                reporter.data_collector.get_cpu()[0].get("cpu_usage").expect("Error In CPU displaying."));
+
         thread::sleep(time::Duration::from_millis(1000));
     });
 
