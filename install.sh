1add1810c2e65f8310ae082e5310320971313961
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-05 21:58:56 +0200

    created simple linux install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+rm -f /bin/xornet
+
+curl --silent "https://api.github.com/repos/xornet-cloud/Reporter/releases/latest"
+  | grep xornet-reporter.linux_x86_64 
+  | grep browser_download_url 
+  | cut -d '"' -f 4 
+  | wget -o /bin/xornet -i - 
