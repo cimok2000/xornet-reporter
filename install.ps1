340409ef554ac14b95d79f93f670405a39423281
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-06 20:09:36 +0200

    finished creating powershell install script

--- install.ps1
+++ install.ps1
@@ -1,42 +1,38 @@
-function curl_check {
-  echo "Checking for curl..."
-  Invoke-WebRequest "https://curl.se/windows/dl-7.80.0/curl-7.80.0-win64-mingw.zip" -o curl.zip
-  Expand-Archive curl.zip
-  Remove-Item curl.zip
-  Move-Item -Path ./curl/curl-7.80.0-win64-mingw/* -Destination ./curl
-  Remove-Item ./curl/curl-7.80.0-win64-mingw
-  New-Item -Path "C:/Program Files" -Name "curl" -ItemType "directory"
-  Move-Item -Path ./curl/* -Destination "C:/Program Files/curl"
-  Create-Item  
-}
-
-function wget_check {
-  echo "Checking for wget..."
-  
-}
-
 function delete_old {
-  # sudo rm -f /usr/local/bin/xornet
-  echo "Deleted old xornet installation"
+  if (Test-Path -Path "C:/Users/$env:UserName/Downloads/bin/xornet.exe") {
+    Remove-Item -Recurse -Force -Confirm:$false "C:/Users/$env:UserName/Downloads/bin/xornet.exe"
+    echo "Deleted old xornet installation"
+  }
+}
+
+function create_bin_folder {
+  if ( Test-Path -Path "C:/Users/$env:UserName/Downloads/bin") {
+    return
+  } else {
+    New-Item -Path "C:/Users/$env:UserName/Downloads" -Name "bin" -ItemType "directory"
+    echo "Created destination bin folder"
+  }
 }
 
 function install {
-  # curl --silent "https://api.github.com/repos/xornet-cloud/Reporter/releases/latest" \
-  # | grep xornet-reporter.linux_x86_64 \
-  # | grep browser_download_url \
-  # | cut -d '"' -f 4 \
-  # | sudo wget -O /usr/local/bin/xornet -i - \
-  # && sudo chmod +x /usr/local/bin/xornet
+
+  create_bin_folder
+
+  $XORNET_LATEST_RELEASES = "https://api.github.com/repos/xornet-cloud/Reporter/releases/latest"   
+  $LATEST_RELEASES_JSON = ((Invoke-WebRequest -UseBasicParsing $XORNET_LATEST_RELEASES) | ConvertFrom-Json).assets.browser_download_url  
+  $WINDOWS_DOWNLOAD_URL = $LATEST_RELEASES_JSON | Select-String -Pattern 'windows' -SimpleMatch
+
+  $WINDOWS_DOWNLOAD_URL = $WINDOWS_DOWNLOAD_URL -replace '\s'
+
+  Invoke-WebRequest -URI $WINDOWS_DOWNLOAD_URL -O "C:/Users/$env:UserName/Downloads/bin/xornet.exe"
+  Set-Item -Path Env:Path -Value ($Env:Path + ";C:/Users/$env:UserName/Downloads/bin/")
 
   echo "Finished installing new version"
 }
 
 function main {
-  curl_check
-  wget_check
   delete_old
   install
-  echo "Installtion finished"
 }
 
 main
