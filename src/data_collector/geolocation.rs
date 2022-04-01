0deb8524d44dde1a04a8ccb454c250dda26d0542
N1kO23 <niko.huuskonen.00@gmail.com>
2022-04-01 22:50:28 +0300

    Added more geolocation data fetching

--- src/data_collector/geolocation.rs
+++ src/data_collector/geolocation.rs
@@ -3,45 +3,25 @@ use serde::{Deserialize, Serialize};
 
 use super::DataCollector;
 
-const IP_ADDRESS_URL: &str = "https://api.ipify.org?format=json";
-const COUNTRY_URL: &str = "https://ipwhois.app/json/";
+const GEOLOCATION_URL: &str = "https://ipwhois.app/json/";
 
 #[derive(Serialize, Deserialize)]
+#[allow(non_snake_case)]
 pub struct GeolocationInfo {
   pub ip: String,
-  pub country: String,
-}
-#[derive(Serialize, Deserialize)]
-struct CurrentIP {
-  ip: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct CurrentCountry {
-  country_code: String,
+  pub country_code: String,
+  pub isp: String,
+  pub city: String,
+  pub timezone_gmtOffset: usize,
 }
 
 impl DataCollector {
-  /// Gets the current public IP address
-  pub async fn get_current_ip() -> Result<String, reqwest::Error> {
-    let cur_ip: String = reqwest::get(IP_ADDRESS_URL)
+  /// Gets the geolocation information
+  pub async fn get_geolocation_info() -> Result<GeolocationInfo, reqwest::Error> {
+    let geolocation_info: GeolocationInfo = reqwest::get(GEOLOCATION_URL)
       .await?
-      .json::<CurrentIP>()
-      .await?
-      .ip;
-    Ok(cur_ip)
-  }
-  /// Gets the current country
-  pub async fn get_current_country(ip: Option<String>) -> Result<String, reqwest::Error> {
-    let cur_ip = match ip {
-      Some(ip) => ip,
-      None => DataCollector::get_current_ip().await?,
-    };
-    let cur_country: String = reqwest::get(COUNTRY_URL.to_owned() + &cur_ip)
-      .await?
-      .json::<CurrentCountry>()
-      .await?
-      .country_code;
-    Ok(cur_country)
+      .json::<GeolocationInfo>()
+      .await?;
+    Ok(geolocation_info)
   }
 }
