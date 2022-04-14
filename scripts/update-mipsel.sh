6dda92c3acd51b3e3a8402ec02dd7be954e859a4
BusteanHAN <b.corbean@student.han.nl>
2022-04-14 11:58:48 +0200

    Fix for existing tmux sessions 2 electric boogaloo

--- scripts/update-mipsel.sh
+++ scripts/update-mipsel.sh
@@ -1,3 +1,4 @@
+tmux send-keys -t 0 C-c "exit" Enter || true
 tmux kill-session -t 0 || true
 cp /tmp/config.json /root || true
 wget https://github.com/xornet-cloud/Reporter/releases/latest/download/xornet-reporter.mipsel-unknown-linux-musl -O /tmp/xornet-reporter 
