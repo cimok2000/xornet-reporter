2c171964ca088081a0ff8c771ea38382a108c901
BusteanHAN <b.corbean@student.han.nl>
2022-04-14 11:56:43 +0200

    Fix for existing tmux sessions

--- scripts/update-mipsel.sh
+++ scripts/update-mipsel.sh
@@ -1,3 +1,4 @@
+tmux kill-session -t 0 || true
 cp /tmp/config.json /root || true
 wget https://github.com/xornet-cloud/Reporter/releases/latest/download/xornet-reporter.mipsel-unknown-linux-musl -O /tmp/xornet-reporter 
 chmod +x /tmp/xornet-reporter
