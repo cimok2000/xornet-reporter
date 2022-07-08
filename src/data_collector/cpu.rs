b31a3a8f1e6bd972dd8b267c865faaba2c97b9f2
Pope Benedict <gruekun@bogos.binted.space>
2022-07-07 23:18:11 -0400

    Stub out windows get_cpu implementation.

--- src/data_collector/cpu.rs
+++ src/data_collector/cpu.rs
@@ -1,9 +1,15 @@
-use crate::types::CPUStats;
-use anyhow::Result;
+#[cfg(target_family = "unix")]
 use sysinfo::{ProcessorExt, SystemExt};
+
+use crate::types::CPUStats;
+
+use anyhow::Result;
+
+
 
 use super::DataCollector;
 
+#[cfg(target_family = "unix")]
 impl DataCollector {
   /// Gets the current CPU stats
   /// wait what the fuck this is an array of cores? 🥴👍
@@ -17,6 +23,16 @@ impl DataCollector {
 
     self.fetcher.refresh_cpu();
 
+    Ok(CPUStats { usage, freq })
+  }
+}
+
+#[cfg(target_family = "windows")]
+impl DataCollector {
+  pub fn get_cpu(&mut self) -> Result<CPUStats> {
+    let (mut usage, mut freq) = (vec![], vec![]);
+    // TODO: Implement use of perfmon querying.
+    //       We need to figure out where to store perfmon structures.
     Ok(CPUStats { usage, freq })
   }
 }
