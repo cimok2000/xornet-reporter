36cedaea248fd96c077053b2529b5c26ff3744d8
Pope Benedict <gruekun@bogos.binted.space>
2022-07-09 16:06:17 -0400

    Finish integration of perfmon for querying CPU frequency/usage data.

--- src/data_collector/mod.rs
+++ src/data_collector/mod.rs
@@ -11,8 +11,11 @@ use crate::types::{DynamicData, StaticData};
 use anyhow::{anyhow, Result};
 use nvml::NVML;
 use std::{collections::HashMap, time::SystemTime};
+use std::process::exit;
 use sysinfo::{ProcessRefreshKind, ProcessorExt, System, SystemExt};
 use thiserror::Error;
+
+use self::gpu::GPUFetcher;
 
 #[cfg(target_family = "windows")]
 use windows::Win32::System::Performance::*;
@@ -21,9 +24,13 @@ use windows::Win32::System::Performance::*;
 use std::ptr;
 
 #[cfg(target_family = "windows")]
-use windows::Win32::Foundation::ERROR_SUCCESS;
+use std::thread::sleep;
 
-use self::gpu::GPUFetcher;
+#[cfg(target_family = "windows")]
+use windows::core::PCSTR;
+
+#[cfg(target_family = "windows")]
+use windows::Win32::Foundation::ERROR_SUCCESS;
 
 #[derive(Error, Debug)]
 pub enum DataCollectorError {
@@ -50,13 +57,19 @@ pub struct DataCollector {
   pub gpu_fetcher: GPUFetcher,
   pub fetcher: System,
   pub program_iterations: usize,
-  pub pdh_query: *mut isize,
-  pub pdh_proc_perf_counter: *mut isize,
-  pub pdh_proc_freq_counter: *mut isize,
-  pub pdh_proc_util_counter: *mut isize,
+  pub pdh_query: isize,
+  pub pdh_proc_perf_counter: isize,
+  pub pdh_proc_freq_counter: isize,
+  pub pdh_proc_util_counter: isize,
   pub pdh_proc_perf_data_cached: *mut PDH_FMT_COUNTERVALUE_ITEM_A,
+  pub pdh_proc_perf_data_len: usize,
+  pub pdh_proc_perf_data_capacity: usize,
   pub pdh_proc_freq_data_cached: *mut PDH_FMT_COUNTERVALUE_ITEM_A,
+  pub pdh_proc_freq_data_len: usize,
+  pub pdh_proc_freq_data_capacity: usize,
   pub pdh_proc_util_data_cached: *mut PDH_FMT_COUNTERVALUE_ITEM_A,
+  pub pdh_proc_util_data_len: usize,
+  pub pdh_proc_util_data_capacity: usize,
   pub first_pdh_called: bool,
   iterator_index: usize,
   network_interface_speeds: HashMap<String, f32>,
@@ -97,49 +110,72 @@ impl DataCollector {
       }
     );
 
-    unsafe {
-      // Initialize all pointers to performance counter resources to NULL.
-      let (pdh_query,
-           pdh_proc_perf_counter,
-           pdh_proc_freq_counter,
-           pdh_proc_util_counter,
-           pdh_proc_perf_data_cached,
-           pdh_proc_freq_data_cached,
-           pdh_proc_util_data_cached) = [ptr::null_mut(); 7];
+    let mut pdh_query = 0 as isize;
+    let mut pdh_proc_perf_counter = 0 as isize;
+    let mut pdh_proc_freq_counter = 0 as isize;
+    let mut pdh_proc_util_counter = 0 as isize;
 
+    let pdh_proc_perf_data_cached = ptr::null_mut();
+    let pdh_proc_freq_data_cached = ptr::null_mut();
+    let pdh_proc_util_data_cached = ptr::null_mut();
+
+    unsafe {
       // Establishes our means to query the winapi performance monitoring system.
-      let ret = PdhOpenQueryA(std::ptr(), 0, pdh_query).ok()?;
-      assert_eq!(ret, ERROR_SUCCESS as i32);
+      let ret = PdhOpenQueryA(PCSTR::default(), 0, &mut pdh_query as *mut isize);
+
+      if ret != ERROR_SUCCESS.0 as i32 {
+        eprintln!("Unable to open perfmon query. Errno: {}", ret);
+        exit(ret);
+      }
 
       // Metric to determine per logical CPU speed relative to base clock of CPU.
       // Example: CPU base frequency is 3500 Mhz and processor performance is 106.
       //          Performance value should be interpreted as a percentage, so in this case
       //          base_freq * (performance / 100) = 3710 Mhz.
-      let ret = PdhAddEnglishCounterA(*pdh_query,
+      let ret = PdhAddEnglishCounterA(pdh_query,
                                       "\\Processor Information(*)\\% Processor Performance",
                                       0,
-                                      pdh_proc_perf_counter).ok()?;
-      assert_eq!(ret, ERROR_SUCCESS as i32);
+                                      &mut pdh_proc_perf_counter as *mut isize);
+
+      if ret != ERROR_SUCCESS.0 as i32 {
+        eprintln!("Unable to add processor performance counter to perfmon query. Errno: {}", ret);
+        exit(ret);
+      }
 
       // Do I really have to say it?
-      let ret = PdhAddEnglishCounterA(*pdh_query,
+      let ret = PdhAddEnglishCounterA(pdh_query,
                                       "\\Processor Information(*)\\Processor Frequency",
                                       0,
-                                      pdh_proc_freq_counter).ok()?;
-      assert_eq!(ret, ERROR_SUCCESS as i32);
+                                      &mut pdh_proc_freq_counter as *mut isize);
+
+      if ret != ERROR_SUCCESS.0 as i32 {
+        eprintln!("Unable to add processor frequency counter to perfmon query. Errno: {}", ret);
+        exit(ret);
+      }
 
       // Processor utilization per core.
-      let ret = PdhAddEnglishCounterA(*pdh_query,
+      let ret = PdhAddEnglishCounterA(pdh_query,
                                       "\\Processor Information(*)\\% Processor Utility",
                                       0,
-                                      pdh_proc_util_counter).ok()?;
-      assert_eq!(ret, ERROR_SUCCESS as i32);
+                                      &mut pdh_proc_util_counter as *mut isize);
+
+      if ret != ERROR_SUCCESS.0 as i32 {
+        eprintln!("Unable to add processor performance utilization to perfmon query. Errno: {}", ret);
+        exit(ret);
+      }
 
       // We have to prime the query data with an initial point of reference. If you
       // don't do this the data retrieved the first time around will either be zeroed or
       // garbage data that won't be useful to you.
-      let ret = PdhCollectQueryData(*pdh_query).ok()?;
-      assert_eq!(ret, ERROR_SUCCESS as i32);
+      let ret = PdhCollectQueryData(pdh_query);
+
+      if ret != ERROR_SUCCESS.0 as i32 {
+        eprintln!("Unable to initialize perfmon queries. Errno: {}", ret);
+        exit(ret);
+      }
+
+      // Guarantees the next invocation of PdhCollectQueryData will yield valid data.
+      sleep(std::time::Duration::from_millis(1000));
     }
 
 
@@ -151,8 +187,14 @@ impl DataCollector {
       pdh_proc_freq_counter,
       pdh_proc_util_counter,
       pdh_proc_perf_data_cached,
+      pdh_proc_perf_data_len: 0,
+      pdh_proc_perf_data_capacity: 0,
       pdh_proc_freq_data_cached,
+      pdh_proc_freq_data_len: 0,
+      pdh_proc_freq_data_capacity: 0,
       pdh_proc_util_data_cached,
+      pdh_proc_util_data_len: 0,
+      pdh_proc_util_data_capacity: 0,
       first_pdh_called: false,
       iterator_index: 0,
       program_iterations: 60,
@@ -172,6 +214,12 @@ impl DataCollector {
   }
 
   pub fn get_all_dynamic_data(&mut self) -> Result<DynamicData> {
+    #[cfg(target_family = "windows")]
+    unsafe {
+      let ret = PdhCollectQueryData(self.pdh_query);
+      assert_eq!(ret, ERROR_SUCCESS.0 as i32);
+    }
+
     Ok(DynamicData {
       cpu: self.get_cpu()?,
       ram: self.get_ram()?,
